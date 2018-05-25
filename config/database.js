7b6741f9025f5b4674ecb566532e02c336910bab
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-05-25 15:38:52 -0700

    back end hooked up

---
+++ config/database.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+
+var db = mongoose.connection; 
+
+db.once('open', function(){
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
+
+db.on('error', function(err){
+    console.error(`Database error:/n${err}`);
+});
